b136a6d51ff982063a39dba7375eff03cffeeb34
Sebastián Giraldo <giraldosebastian99@gmail.com>
2022-04-05 14:57:58 -0500

    Version updated

--- lib/src/constants/constants.dart
+++ lib/src/constants/constants.dart
@@ -1,6 +1,6 @@
 class Constants {
   /// Version used for the 'User-Agent' header.
-  static const packageVersion = '0.0.2';
+  static const packageVersion = '0.0.3';
 
   /// Used to test http calls with mock data.
   static const mockHttpForTesting = true;
